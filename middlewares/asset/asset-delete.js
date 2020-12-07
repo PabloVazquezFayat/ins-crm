6156f6c43c5f68a4f61084e20b73cd5f4862bfe4
Pablo Vazquez <pvazquez@vandldigital.com>
2020-12-07 18:35:27 -0500

    asset-delete middleware

--- middlewares/asset/asset-delete.js
+++ middlewares/asset/asset-delete.js
@@ -1,18 +1,28 @@
 const cloudinary = require('cloudinary');
+const Asset = require('../../models/Asset');
 const config = require('./config');
 
 module.exports = (req, res, req)=> {
 
     cloudinary.config(config);
 
-    cloudinary.v2.uploader.destroy(req.body.public_id, {resource_type: "auto", invalidate: true}, (error, result)=>{
-        if(error){
-            req.body.asset = error;
-            next();
-        }else{
-            req.body.asset = result;
-            next();
+    try{
+
+        const asset = await Asset.findOne({_id: req.body.data.id});
+
+        if(asset){
+            cloudinary.v2.uploader.destroy(asset.name, {resource_type: "auto", invalidate: true}, (error, result)=>{
+                if(error){
+                    res.status(500).json({error: error})
+                }else{
+                    req.body.asset = result;
+                    next();
+                }
+            });
         }
-    });
+
+    }catch(error){
+        next();
+    }
 
 }
